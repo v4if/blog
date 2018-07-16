ab38f500453596955e9adf40825cc7a42ad225ef
show hand <atkjest@gmail.com>
2018-07-16 11:31:25 +0800

    Update shLint.sh

--- commonconfig/shLint.sh
+++ commonconfig/shLint.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 set -eo pipefail
+
+
+'-c string If  the  -c  option  is  present, then commands are read from
+          string.  If there are arguments after the  string,  they  are
+          assigned to the positional parameters, starting with $0.
+         
+ sh -c ls'
