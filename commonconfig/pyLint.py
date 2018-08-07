1639666d1bd20186dc913e38d775821e7b3f6c09
show hand <atkjest@gmail.com>
2018-08-07 20:36:09 +0800

    Update pyLint.py

--- commonconfig/pyLint.py
+++ commonconfig/pyLint.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# creating a dictionary of lists
+>>> from collections import defaultdict
+>>> d = defaultdict(list)
+>>> for i in a:
+...   for j in range(int(i), int(i) + 2):
+...     d[j].append(i)
+...
+>>> d
+defaultdict(<type 'list'>, {1: ['1'], 2: ['1', '2'], 3: ['2']})
+>>> d.items()
+[(1, ['1']), (2, ['1', '2']), (3, ['2'])]
